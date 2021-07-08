a29f190109253ddc16a39447e5155dec4606d637
KellyLoveyness <86350461+KellyLoveyness@users.noreply.github.com>
2021-07-08 20:32:22 +0100

    Update resetdifficulty.js

--- js/QuickAccessMenuCommands/resetdifficulty.js
+++ js/QuickAccessMenuCommands/resetdifficulty.js
@@ -4,7 +4,7 @@ QuickAccessMenu.push({
 	alias: ["difficultyreset"],
 	help: "Quick-AccessMenu2.2: Command can reset difficulty, to end extreme mode instantly for example.",
 	execute(argument) {
-        if (argument.includes("yes")) {
+        if (argument[0] == "yes") {
         Player.Difficulty = [];
         ChatRoomMessage({ Content: "Quick-AccessMenu2.2: Difficulty reset, select a new one in settings.", Type: "LocalMessage", Sender: Player.MemberNumber });}
         else {ChatRoomMessage({ Content: "Quick-AccessMenu2.2: Warning, resetting difficulty will incur a 7-day waiting period to rechange. Confirm by typing: /resetdifficulty yes", Type: "LocalMessage", Sender: Player.MemberNumber })}
